8f1a714e00e94d8fff8e5d2628d138c32c626924
Nura730 <arunshanmugavel3726@gmail.com>
2026-06-13 08:51:58 +0530

    feat: semester loading

--- src/store/semesterStore.ts
+++ src/store/semesterStore.ts
@@ -1,19 +1,29 @@
 import { create } from "zustand";
-import { Semester } from "../types/semester";
+
 import {
   doc,
   setDoc,
+  collection,
+  getDocs,
 } from "firebase/firestore";
 
 import { db } from "../services/firebase";
 
+import { Semester } from "../types/semester";
+
 interface SemesterState {
-createSemester: (
-  semester: Semester,
-  uid: string
-) => Promise<void>;
   semester: Semester | null;
+
   loading: boolean;
+
+  createSemester: (
+    semester: Semester,
+    uid: string
+  ) => Promise<void>;
+
+  loadSemester: (
+    uid: string
+  ) => Promise<void>;
 
   setSemester: (
     semester: Semester | null
@@ -24,38 +34,86 @@ createSemester: (
 
 export const useSemesterStore =
   create<SemesterState>((set) => ({
-    createSemester: async (
-  semester,
-  uid
-) => {
-  set({ loading: true });
-
-  try {
-    await setDoc(
-      doc(
-  db,
-  "users",
-  uid,
-  "semesters",
-  semester.id
-),
-      semester
-    );
-
-    set({
-      semester,
-    });
-  } finally {
-    set({ loading: false });
-  }
-},
     semester: null,
 
     loading: false,
 
-    setSemester: (semester) =>
-      set({ semester }),
+    createSemester: async (
+      semester,
+      uid
+    ) => {
+      set({ loading: true });
+
+      try {
+        await setDoc(
+          doc(
+            db,
+            "users",
+            uid,
+            "semesters",
+            semester.id
+          ),
+          semester
+        );
+
+        set({
+          semester,
+        });
+      } finally {
+        set({
+          loading: false,
+        });
+      }
+    },
+
+    loadSemester: async (
+      uid
+    ) => {
+      set({ loading: true });
+
+      try {
+        const snapshot =
+          await getDocs(
+            collection(
+              db,
+              "users",
+              uid,
+              "semesters"
+            )
+          );
+
+        const semesters =
+          snapshot.docs.map(
+            (doc) =>
+              doc.data() as Semester
+          );
+
+        const activeSemester =
+          semesters.find(
+            (semester) =>
+              semester.isActive
+          );
+
+        set({
+          semester:
+            activeSemester || null,
+        });
+      } finally {
+        set({
+          loading: false,
+        });
+      }
+    },
+
+    setSemester: (
+      semester
+    ) =>
+      set({
+        semester,
+      }),
 
     clearSemester: () =>
-      set({ semester: null }),
+      set({
+        semester: null,
+      }),
   }));
